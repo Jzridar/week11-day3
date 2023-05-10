1e1f1713bf5be63175aa0bad7c11dc10aa04e887
Jzridar <jzridar@gmail.com>
2023-05-10 14:32:01 -0400

    files

---
+++ server.js
@@ -0,0 +1,52 @@
+//bring in express
+const express = require('express')
+
+//instantiate express
+const app = express();
+
+const port= 3000
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('jsx-view-engine').createEngine());
+
+app.get('/', (req,res) => {
+    res.send('<h1> Home page,Route 1 </h1>')
+})
+
+app.get("/route2",(req,res) => {
+    res.render('Route2')
+})
+app.get('/3', (req,res) => {
+    res.send('<h1> Route 3</h1>')
+})
+app.get('/4', (req,res) => {
+    res.send('<h1> Route 4</h1>')
+})
+app.get('/5', (req,res) => {
+    res.send('<h1> Route 5</h1>')
+})
+app.get('/6', (req,res) => {
+    res.send('<h1> Route 6</h1>')
+})
+app.get('/7', (req,res) => {
+    res.send('<h1> Route 7</h1>')
+})
+app.get('/8', (req,res) => {
+    res.send('<h1> Route 8</h1>')
+})
+app.get('/9', (req,res) => {
+    res.send('<h1> Route 9</h1>')
+})
+app.get('/10', (req,res) => {
+    res.send('<h1> Route 10</h1>')
+})
+
+
+
+
+
+
+//Tell app to listen to the port
+app.listen(port,()=> {
+    console.log(`listening on port, ${port}`)
+})
---
+++ views/Route2.jsx
@@ -0,0 +1,10 @@
+const React = require('react')
+
+class Index extends React.Component{
+    render() {
+        return(
+            <h1>Route 2/Template</h1>
+        )
+    }
+}
+module.exports = Index
